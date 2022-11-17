1b85d2de7a6ba440b473a021e2dea7ed682eeebe
Georgia <georgiab543@gmail.com>
2022-11-17 18:42:36 -0300

    requisito 3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions.js';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 /* requisito 2 */
@@ -9,17 +9,21 @@ const searchButton = document.querySelector('#search-products');
 const inputSearch = document.querySelector('#products-input');
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
-searchButton.addEventListener('click', async () => {
-  /* console.log('computador'); */
-  await fetchProductsList(inputSearch.value);
-  
+
+const getProducts = async (value) => {
+  const products = await fetchProductsList(value);
   const sectionProducts = document.querySelector('.products');
-  const products = await fetchProductsList('computador');
+  sectionProducts.innerHTML = '';
   /* console.log(products); */
   products.forEach((product) => {
-    const { id, title, thumbnail, price } = product;
-    /* console.log({ id, title, thumbnail, price }); */
-    sectionProducts.appendChild(createProductElement({ id, title, thumbnail, price }));
-    })
-  
+    sectionProducts.appendChild(createProductElement(product));
+  });
+};
+
+window.onload = async () => {
+  await getProducts('computador');
+};
+
+searchButton.addEventListener('click', async () => {
+  await getProducts(inputSearch.value);
 });
