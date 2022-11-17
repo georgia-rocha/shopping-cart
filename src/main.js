eb02639486b5b638f4330a20ba4935ba7560dd56
Georgia <georgiab543@gmail.com>
2022-11-17 17:04:01 -0300

    requisito 3, roda + n passa

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions.js';
 import './style.css';
+
+/* requisito 2 */
 
 const searchButton = document.querySelector('#search-products');
 const inputSearch = document.querySelector('#products-input');
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 searchButton.addEventListener('click', async () => {
-  console.log('computador');
+  /* console.log('computador'); */
   await fetchProductsList(inputSearch.value);
+  
+  const sectionProducts = document.querySelector('.products');
+  const products = await fetchProductsList('computador');
+  /* console.log(products); */
+  products.forEach((product) => {
+    const { id, title, thumbnail, price } = product;
+    /* console.log({ id, title, thumbnail, price }); */
+    sectionProducts.appendChild(createProductElement({ id, title, thumbnail, price }));
+    })
+  
 });
