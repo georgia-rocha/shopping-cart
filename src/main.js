f60a77252b52817efe58c1f596e2d3338e53f8ba
Georgia <georgiab543@gmail.com>
2022-11-17 21:22:42 -0300

    requisito 4

--- src/main.js
+++ src/main.js
@@ -3,16 +3,20 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
-/* requisito 2 */
-
 const searchButton = document.querySelector('#search-products');
 const inputSearch = document.querySelector('#products-input');
+const sectionProducts = document.querySelector('.products');
+
+const loading = document.createElement('h2');
+loading.innerHTML = 'carregando...';
+loading.className = 'loading';
+sectionProducts.appendChild(loading);
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const getProducts = async (value) => {
   const products = await fetchProductsList(value);
-  const sectionProducts = document.querySelector('.products');
+
   sectionProducts.innerHTML = '';
   /* console.log(products); */
   products.forEach((product) => {
